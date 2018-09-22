c58fc12bdef7212f04c90dbbf5244bec065c2292
taylorreece <taylor.reece@gmail.com>
2018-09-22 15:02:51 -0500

    Added option to display beats of a song

--- cli/sabergen_cli.py
+++ cli/sabergen_cli.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
-from libsabergen import sabergen
+from libsabergen.sabergen import beatSaberSong
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input', dest='input_path',
-                        help='Path to input music file', required=True)
+    parser.add_argument('-i', '--input-path', help='Path to input music file', required=True)
+    parser.add_argument('-p', '--pyplot', action='store_true', help='Display song as pyplot')
+    # Todo: subparsers rather than --pyplot or --show-beats; makes args easier to sort
+    parser.add_argument('-b', '--show-beats', action='store_true', help='Show beats upon which there is a beat')
+    parser.add_argument('--bpm', type=int, help='Beats per minute', default=120)
 
     args = parser.parse_args()
 
-    sabergen.display_song_as_pyplot(args.input_path)
+    beat_saber_song = beatSaberSong(args.input_path)
+
+    if args.pyplot:
+        beat_saber_song.display_song_as_pyplot()
+    if args.show_beats:
+        print(beat_saber_song.get_beats(args.bpm))
+
 
 if __name__ == '__main__':
     main()
--- libsabergen/sabergen.py
+++ libsabergen/sabergen.py
@@ -10,18 +10,26 @@ from matplotlib import pyplot as plt
 import librosa
 import librosa.display as disp
 
-def display_song_as_pyplot(song_path):
-    y, sr = librosa.load(song_path)
-
-    # Compute spectrogram magnitude and phase
-    S_full, phase = librosa.magphase(librosa.stft(y))
-
-    idx = slice(*librosa.time_to_frames([30, 35], sr=sr))
-    plt.figure(figsize=(12, 4))
-    disp.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
-                  y_axis='log', x_axis='time', sr=sr)
-    plt.colorbar()
-    plt.tight_layout()
-
-    plt.show()
+class beatSaberSong(object):
+    def __init__(self, song_path):
+        self.song_path = song_path
     
+    def display_song_as_pyplot(self):
+        y, sr = librosa.load(self.song_path)
+
+        # Compute spectrogram magnitude and phase
+        S_full, phase = librosa.magphase(librosa.stft(y))
+
+        idx = slice(*librosa.time_to_frames([30, 35], sr=sr))
+        plt.figure(figsize=(12, 4))
+        disp.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+                    y_axis='log', x_axis='time', sr=sr)
+        plt.colorbar()
+        plt.tight_layout()
+
+        plt.show()
+
+    def get_beats(self, bpm=120):
+        y, sr = librosa.load(self.song_path)
+        tempo, beats = librosa.beat.beat_track(y, sr, bpm=bpm)
+        return beats
