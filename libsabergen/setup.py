32301a30897886ae605626a29b2b79cc729660d3
Ryan Wersal <ryan@ryanwersal.com>
2018-10-02 21:14:02 -0500

    Enable Python 3.5 and up for builds/testing
    Moves knowledge of required version from pipenv to setup.py

--- libsabergen/setup.py
+++ libsabergen/setup.py
@@ -7,4 +7,13 @@ setuptools.setup(
     author_email="",
     description="libsabergen description",
     packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
