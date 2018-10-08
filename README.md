3c4cae788c9da612f61813039f239d31ac4697d3
Ryan Wersal <ryan@ryanwersal.com>
2018-10-08 12:14:35 -0500

    Restructure lib/cli relationship
    This change moves more of the concerns into the library (it seems better to have the library
    enforce the ogg format requirement than have all callers mandate it). It still leaves the
    same public API surface in case the caller does want to change the behavior but load now
    handles the main concern (only ogg format past a certain point).
    
    Also add more details to the wiki for getting ffmpeg successfully installed with the necessary
    codecs etc. It is probably we will need to add more in the future (the ones included are the
    ones I needed to successfully convert an mp3 I was testing with).

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 # sabergen
+
 Beat Saber song generator
 
-# Installation / Development
-## Installing Python/Pipenv
-You probably already have python.  Just make sure you have 3.7.\*.  Only chumps use 2.\*
+## Installation / Development
+
+### Installing Python/Pipenv
+
+You probably already have python.  Just make sure you have 3.5 or higher.  Only chumps use 2.\*
 To get pipenv on OSX, you'll need xcode-select, then brew install pipenv.
-```
-$ xcode-select --install
-$ brew install pipenv
-```
-For other operating systems, see https://pipenv.readthedocs.io/en/latest/install/#installing-pipenv
-## Installing python and dependencies
-Once pipenv is installed,
-```
-$ cd /path/to/sabergen/
-$ pipenv install
+
+```bash
+xcode-select --install
+brew install pipenv
 ```
 
-You'll also need ffmpeg
+For other operating systems, see [pipenv's documentation](https://pipenv.readthedocs.io/en/latest/install/#installing-pipenv).
+
+### Installing python and dependencies
+
+Once pipenv is installed,
+
+```bash
+cd /path/to/sabergen/
+pipenv install --dev
 ```
-http://www.ffmpeg.org/download.html
+
+You'll also need [ffmpeg](https://www.ffmpeg.org/download.html). Note that you will need
+to have the libvorbis encoder.
+
+Installing on macOS using `brew`:
+
+```bash
+brew install ffmpeg --with-theora --with-libvorbis --with-libvpx
 ```
--- cli/sabergen_cli.py
+++ cli/sabergen_cli.py
@@ -10,24 +10,21 @@ def main():
     parser.add_argument('-p', '--pyplot', action='store_true', help='Display song as pyplot')
     # Todo: subparsers rather than --pyplot or --show-beats; makes args easier to sort
     parser.add_argument('-b', '--show-beats', action='store_true', help='Show beats upon which there is a beat')
-    parser.add_argument('--bpm', type=int, help='Beats per minute', default=120)
+    parser.add_argument('--bpm', type=int, help='Estimated beats per minute', default=120)
     parser.add_argument('-d', '--debug', action='store_true', help='Print debug info')
 
     args = parser.parse_args()
     
-    beat_saber_song = beatSaberSong(args.input_path, args.debug)
-
-    # We only handle ogg from here
-    if not beat_saber_song.is_ogg(beat_saber_song.song_path):
-        print('Input specified is not an ogg. Converting to mp3')
-        beat_saber_song.convert_to_ogg(beat_saber_song.song_path)
+    song = beatSaberSong(args.debug)
+    song.load(args.input_path)
 
     if args.pyplot:
         print('Plotting song...')
-        beat_saber_song.display_song_as_pyplot()
+        song.display_song_as_pyplot()
+        
     if args.show_beats:
         print('Genearting BPM...')
-        print(beat_saber_song.get_beats(args.bpm))
+        print(song.get_beats(args.bpm))
 
 if __name__ == '__main__':
     main()
