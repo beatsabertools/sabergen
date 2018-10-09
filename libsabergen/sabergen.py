83a7c5a539ea0708daf650acef55d08e46b3e3b8
Ryan Wersal <ryan@ryanwersal.com>
2018-10-08 23:42:04 -0500

    Enable pylint and refactoring tooling
    Also adds it to run during the build

--- libsabergen/sabergen.py
+++ libsabergen/sabergen.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
 
-import numpy as np
+"""
+sabergen library to drive song generation for Beat Saber
+"""
 
-# Configure matplotlib for use with virtualenv
-import matplotlib
-matplotlib.use('TkAgg')
-from matplotlib import pyplot as plt
+import numpy as np
 
 import librosa
 import librosa.display as disp
 
-class beatSaberSong(object):
+# Configure matplotlib for use with virtualenv
+import matplotlib
+matplotlib.use('TkAgg')
+# Ignore the pylint error as we need to specify, explicitly, a backend
+# pylint: disable=wrong-import-position
+from matplotlib import pyplot as plt
+
+class BeatSaberSong:
+    """
+    Represents container of knowledge and logic for converting a provided
+    audio file into a Beat Saber song.
+    """
+
     def __init__(self, debug=False):
         self.debug = debug
-        self.y = None
-        self.sr = None
+        self.time_series = None
+        self.sampling_rate = None
         self.audio_path = None
 
     def load(self, audio_path):
@@ -24,23 +35,23 @@ class beatSaberSong(object):
         if not self.is_ogg(audio_path):
             self.audio_path = self.convert_to_ogg(audio_path)
 
-        self.y, self.sr = librosa.load(self.audio_path)
+        self.time_series, self.sampling_rate = librosa.load(self.audio_path)
 
     def is_ogg(self, song_path):
         "Using the magic number of a file, determine if this is an .ogg file."
-        
+
         # Assuming we aren't opening a huge file.
         with open(song_path, "rb") as song:
             # Load the first 4 bytes
             song_bytes = song.read(4)
-            
+
             # Turn those hex bytes into a string.
             hex_bytes = ''.join(['{:02X}'.format(byte) for byte in song_bytes])
-            
+
             # Magic number of ogg is OggS in ASCII
             if hex_bytes == '4F676753':
                 return True
-        
+
         return False
 
     def convert_to_ogg(self, song_path):
@@ -55,17 +66,17 @@ class beatSaberSong(object):
         # This is already a slower call.
         import subprocess
         from os import path
-        
+
         try:
             # Setup new file name and path.
             filename, _ = path.splitext(path.basename(song_path))
-            ogg_filename = "{}.ogg".format(filename)    
+            ogg_filename = "{}.ogg".format(filename)
             ogg_path = path.join(path.dirname(song_path), ogg_filename)
 
             # Command will generate a new file with the same name, except .ogg
             # -c:a codec, -y overwrite if output exists, -q quality specifier
             subprocess.run("ffmpeg -y -i {} -c:a libvorbis -q:a 8 {}".format(song_path, ogg_path),
-                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+                           shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
 
             if self.debug:
                 # TODO: How best to return stdout/stderr from a library?
@@ -73,27 +84,24 @@ class beatSaberSong(object):
 
             return ogg_path
 
-        except subprocess.CalledProcessError as e:
+        except subprocess.CalledProcessError as err:
             # FIXME: Need to find a better way to return a result object to callers
             # since we likely shouldn't blindly produce stdout/stderr. Perhaps take in a logger?
             # What logging libraries are popular in Python?
-            print(e.args)
-            print(e.stderr)
-            print(e.stdout)
+            print(err.args)
+            print(err.stderr)
+            print(err.stdout)
             raise
 
-        except Exception:
-            raise
-    
     def display_song_as_pyplot(self):
         "Plots spectrogram magnitude using matplotlib."
         # Compute spectrogram magnitude and phase
-        S_full, _ = librosa.magphase(librosa.stft(self.y))
+        s_full, _ = librosa.magphase(librosa.stft(self.time_series))
 
-        idx = slice(*librosa.time_to_frames([30, 35], sr=self.sr))
+        idx = slice(*librosa.time_to_frames([30, 35], sr=self.sampling_rate))
         plt.figure(figsize=(12, 4))
-        disp.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
-                    y_axis='log', x_axis='time', sr=self.sr)
+        disp.specshow(librosa.amplitude_to_db(s_full[:, idx], ref=np.max),
+                      y_axis='log', x_axis='time', sr=self.sampling_rate)
         plt.colorbar()
         plt.tight_layout()
 
@@ -101,6 +109,7 @@ class beatSaberSong(object):
 
     def get_beats(self, bpm=120):
         "Identify beat timestamps in the audio."
-        _, beat_frames = librosa.beat.beat_track(self.y, self.sr, bpm=bpm)
-        beat_times = librosa.frames_to_time(beat_frames, sr=self.sr)
+        _, beat_frames = librosa.beat.beat_track(
+            self.time_series, self.sampling_rate, bpm=bpm)
+        beat_times = librosa.frames_to_time(beat_frames, sr=self.sampling_rate)
         return beat_times
